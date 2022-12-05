82641caa6816f2fc5e9c8df4665d7bae4c7fcd83
xrz <2895538112@qq.com>
2022-12-05 22:02:29 +0800

    1.成功实现棋盘随机且符合规则的生成 2.初步实现规则限制下的棋子移动（已修复bug:炮现在可以吃没翻开的棋子了） 3.实现一键重开功能 4.增加音乐播放模块

---
+++ src/Media/MusicStuff.java
@@ -0,0 +1,31 @@
+package Media;
+import javax.sound.sampled.*;
+import java.io.File;
+public class MusicStuff
+{
+    public void playMusic(String musicLocation)
+    {
+        try
+        {
+            File musicPath = new File(musicLocation);
+            if (musicPath.exists())
+            {
+                AudioInputStream audioInput = AudioSystem.getAudioInputStream(musicPath);
+                Clip clip = AudioSystem.getClip();
+                clip.open(audioInput);
+                clip.start();
+                clip.loop(Clip.LOOP_CONTINUOUSLY);
+            }
+        }
+        catch(Exception ex)
+        {
+            ex.printStackTrace();
+        }
+    }
+}
+/*
+    String filepath ="海德薇变奏曲.wav";
+    MusicStuff musicObject = new MusicStuff();
+    musicObject.playMusic(filepath);
+//播放方式
+*/
